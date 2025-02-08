904ef97ea1c9eac33a0cc0d4e9bc1e2691c7417d
Gus Cost <guscost@gmail.com>
2025-02-08 13:10:59 -0500

    Update tailwind config

--- storybook-static/assets/entry-preview-Bpm1nvU0.js
+++ storybook-static/assets/entry-preview-Bpm1nvU0.js
@@ -1,5 +1,5 @@
 const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./react-18-LWG_bOUi.js","./index-CYQpqK1Q.js","./_commonjsHelpers-CqkleIqs.js","./index-CjktTq3Q.js"])))=>i.map(i=>d[i]);
-import{_ as Oe}from"./iframe-BKwR0puE.js";import{_ as je,a as N,d as Pe}from"./chunk-XP5HYGXS-D5tuasO7.js";import{a as Ce,r as k,R as Y}from"./index-CYQpqK1Q.js";import"./_commonjsHelpers-CqkleIqs.js";var le={exports:{}},oe={};/**
+import{_ as Oe}from"./iframe-Uul3Fz9o.js";import{_ as je,a as N,d as Pe}from"./chunk-XP5HYGXS-D5tuasO7.js";import{a as Ce,r as k,R as Y}from"./index-CYQpqK1Q.js";import"./_commonjsHelpers-CqkleIqs.js";var le={exports:{}},oe={};/**
  * @license React
  * react-dom-test-utils.production.js
  *
